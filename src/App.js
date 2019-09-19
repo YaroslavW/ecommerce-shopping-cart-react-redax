c1f34f46b33ad8596586d39624e42b2ba944b5a3
Yaroslav <doctor355@yahoo.com>
2019-09-19 14:33:20 +0300

    Filter component start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Products from './components/Poducts'
+import Filter from './components/Filter'
 import './App.css';
 
 export default class Poducts extends Component {
@@ -25,6 +26,14 @@ export default class Poducts extends Component {
           <hr />
           <div className="row">
             <div className="col-md-8">
+              <Filter
+                size={this.state.size}
+                sort={this.state.sort}
+                handleChangeSize={this.handleChangeSize}
+                handleChangeSort={this.handleChangeSort}
+                count={this.state.filteredProducts.length}
+              />
+              <hr/>
               <Products
                 products={this.state.filteredProducts}
                 handleAddToCard={this.handleAddToCard}
